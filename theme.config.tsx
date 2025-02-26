cc169fa2517ba14812eeabf7de0c3d776c526268
kota-yata <kota@yatagai.com>
2025-02-26 22:03:19 +0900

    organize files

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,24 +1,8 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
-import { useRouter } from "next/router";
-
-const HeadComponent = () => {
-    const router = useRouter();
-    const src = `${router.basePath}/static/lawtext_bundles/browser/lawtext.js`;
-    const fallbackSrc = "https://yamachig.github.io/Lawtext/static/lawtext_bundles/browser/lawtext.js";
-    React.useEffect(() => {
-        import(/*webpackIgnore: true*/ src)
-            .catch(() => {
-                return import(/*webpackIgnore: true*/ fallbackSrc);
-            })
-            .catch(console.error);
-    }, [src]);
-    return null;
-};
 
 const config: DocsThemeConfig = {
     logo: <span>拡張版法令標準XMLスキーマ</span>,
-    head: HeadComponent,
     editLink: { component: () => null },
     feedback: { content: () => null },
     footer: { text: () => (<div><small>

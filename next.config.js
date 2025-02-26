8f5ceea855411d4d1b985a66e5b8738a6a6f3db9
kota-yata <kota@yatagai.com>
2025-02-26 22:17:20 +0900

    update configs

--- next.config.js
+++ next.config.js
@@ -4,17 +4,12 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const nextra = require("nextra");
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const baseConfig = require("./base.next.config");
-
-
 const withNextra = nextra({
     theme: "nextra-theme-docs",
     themeConfig: "./theme.config.tsx",
 });
 
 const config = {
-    ...baseConfig,
     output: "export",
     images: {
         unoptimized: true,
@@ -22,4 +17,3 @@ const config = {
     trailingSlash: true,
 };
 module.exports = withNextra(config);
-
--- theme.config.tsx
+++ theme.config.tsx
@@ -3,14 +3,8 @@ import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
     logo: <span>拡張版法令標準XMLスキーマ</span>,
-    editLink: { component: () => null },
-    feedback: { content: () => null },
-    footer: { text: () => (<div><small>
-        &copy; {new Date().getFullYear()} nozawa & kota-yata
-    </small></div>) },
+    footer: { text: () => (<span>© 2025 nozawa & kota-yata</span>) },
     faviconGlyph: "K",
-    gitTimestamp: () => null,
-    useNextSeoProps: () => ({ titleTemplate: "%s - Lawtext-notice" }),
     nextThemes: {
         defaultTheme: "dark",
     },
